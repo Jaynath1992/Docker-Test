18e806adf460379c702812261faed46bbf149194
Jaynath1992 <jaynath120.kumar@gmail.com>
2019-04-22 00:40:06 +0530

    Create python_test.py

---
+++ python_test.py
@@ -0,0 +1,13 @@
+def add(a, b):
+  print 'sum is : {}'.format(a+b)
+  
+def sub(a, b):
+  print 'Sub is : {}'.format(a-b)
+  
+def mul(a, b):
+  print 'Multiplication is :'.format(a*b)
+
+if __name__ == '__main__':
+  sum = add(10,20)
+  sub = sub(30,10)
+  mul = mul(20,10)
